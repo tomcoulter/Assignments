c1cd96b1f35f68b4511d22a989d512d142e9dd2e
tomcoulter <35543461+tomcoulter@users.noreply.github.com>
2019-04-29 17:40:07 -0500

    Create scrapingassignment3.py

---
+++ scrapingassignment3.py
@@ -0,0 +1,26 @@
+import urllib2, csv
+from bs4 import BeautifulSoup
+
+csvfile = open('billtest.csv', 'a')
+billtest_writer = csv.writer(csvfile)
+
+url = 'https://www.senate.mo.gov/19info/BTS_Web/TrulyAgreed.aspx?SessionType=R'
+html = urllib2.urlopen(url).read()
+
+soup = BeautifulSoup(html, 'html.parser')
+
+tables = soup.find_all('table', {'id': 'Table2'})
+
+for Table2 in tables:
+    
+    rows = Table2.find_all('tr')
+
+    for row in rows:
+        output_row = []
+
+        cells = row.find_all('td')
+
+        for cell in cells:
+            output_row.append(cell.text)
+
+    billtest_writer.writerow(output_row)
